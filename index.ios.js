214a63466a40f034aaab5b47fd3fb898c1221e22
Anthony Reinbach <areinbach@gmail.com>
2015-08-17 14:08:03 +1000

    Switch base page to ES6 classes

--- index.ios.js
+++ index.ios.js
@@ -12,24 +12,25 @@ var {
   View,
 } = React;
 
-var mobileloci = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!!!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+class mobileloci extends React.Component{
+    render()
+    {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to Daaaa React Native!!!!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
+};
 
 var styles = StyleSheet.create({
   container: {
