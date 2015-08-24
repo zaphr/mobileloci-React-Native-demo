114a64e06254f4c737f1fe24afa6da7feed52e37
Anthony Reinbach <areinbach@gmail.com>
2015-08-24 15:59:03 +1000

    Add images

---
+++ App/Components/Main.js
@@ -0,0 +1,77 @@
+var React = require('react-native');
+
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    NavigatorIOS,
+    Image
+    } = React;
+
+class Main extends React.Component {
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+            <Text style={styles.title}>This is main</Text>
+                <Image style={styles.image}
+                       source={require('image!cat')}  />
+            </View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        padding: 30,
+        marginTop: 65,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#48BBEC'
+    },
+    title: {
+        marginBottom: 20,
+        fontSize: 25,
+        textAlign: 'center',
+        color: '#fff'
+    },
+    image: {
+        height: 125,
+        width: 125,
+        borderRadius: 65,
+        marginTop: 10,
+        alignSelf: 'center'
+    },
+
+
+    searchInput: {
+        height: 50,
+        padding: 4,
+        marginRight: 5,
+        fontSize: 23,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        color: 'white'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#111',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 45,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        marginTop: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+});
+
+module.exports = Main;
